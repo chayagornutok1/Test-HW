3c12beb1c1606bef08f8819b752f2647a81c71b6
chayagorn.utok <cam_lasand@hotmail.com>
2017-04-22 16:52:54 +0700

    9

--- Chayagorn.utok_1570700565/Assets/Scene_/Scripts/DestoryBycontant.cs
+++ Chayagorn.utok_1570700565/Assets/Scene_/Scripts/DestoryBycontant.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class DestoryBycontant : MonoBehaviour {
 
-	void OnTriggerEnter(Collider other){
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") 
+		{
+			return;		
+		}
+		Instantiate(explosion,transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate(playerExplosion,other.transform.position, other.transform.rotation);
+		}
+
+
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
